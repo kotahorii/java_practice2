a193c247c1198d02bdbfac338118a1969375d215
kotahorii <khorii.micron@gmail.com>
2022-03-07 14:11:10 +0900

    [Update] file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,12 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        boolean is_prime = true;
-        if (num % 2 == 0) {
-            System.out.println("素数でない");
-            is_prime = false;
-        } else {
-            for (int i = 3; i < Math.sqrt(num) + 1; i += 2) {
-                if (num % i == 0) {
-                    System.out.println("素数でない");
-                    is_prime = false;
-                    break;
-                }
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.printf("%d * %d =%2d, ", j, i, i * j);
             }
-        }
-        if (is_prime == true) {
-            System.out.println("素数");
+            System.out.println();
         }
     }
 }
