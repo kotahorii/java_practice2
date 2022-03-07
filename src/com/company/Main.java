52716e9a6809aa447b9b73a0087fd06551875ffb
kotahorii <khorii.micron@gmail.com>
2022-03-07 13:41:00 +0900

    [Update] file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,20 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        int sum = 0;
+        int strike_count = 0;
+        int ball_count = 0;
         Scanner scanner = new Scanner(System.in);
-        while (sum < 100) {
+
+        while (strike_count < 3 && ball_count < 4) {
             int num = scanner.nextInt();
-            sum += num;
+
+            if (num == 1) {
+                strike_count += 1;
+            } else if (num == 2) {
+                ball_count += 1;
+            }
         }
-        System.out.println(sum);
+        System.out.println(ball_count + "ボール、" + strike_count + "ストライク");
     }
 }
