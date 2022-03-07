7c40a8341f25313b395f22f1d0b6a6c0493589c3
kotahorii <khorii.micron@gmail.com>
2022-03-07 13:23:40 +0900

    [Update] file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int kyojin_sum = 0;
-        int hanshin_sum = 0;
-        String winner;
-        for (int i = 1; i < 10; i++) {
-            int kyojin_point = scan.nextInt();
-            int hanshin_point = scan.nextInt();
-
-            kyojin_sum += kyojin_point;
-            hanshin_sum += hanshin_point;
+        int max_num = 0;
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            int input_num = scanner.nextInt();
+            if (max_num == 0) {
+                max_num = input_num;
+            } else {
+                if (input_num > max_num) {
+                    max_num = input_num;
+                }
+            }
         }
-        if (kyojin_sum > hanshin_sum) {
-            winner = "巨人";
-        } else {
-            winner = "阪神";
-        }
-        System.out.println("巨人：" + kyojin_sum + "点" + "阪神：" + hanshin_sum + "点");
-        System.out.println(winner + "の勝ち");
+        System.out.println(max_num);
     }
 }
