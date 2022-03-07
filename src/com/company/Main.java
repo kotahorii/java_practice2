ce659a95d003aa8655d73f4d76791512ca2148e5
kotahorii <khorii.micron@gmail.com>
2022-03-07 14:22:38 +0900

    [Update] file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,18 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean is_stop = false;
-        int sum = 0;
-        int count = 0;
-        while (!is_stop) {
-            int num = scanner.nextInt();
-            if (num == 0 && count != 0) {
-                is_stop = true;
-            } else {
-                sum += num;
-                count += 1;
+        int num = scanner.nextInt();
+
+        for (int i = 1; i < num + 1; i++) {
+            for (int j = 1; j < i + 1; j++) {
+                System.out.printf("$");
             }
+            System.out.println();
         }
-        System.out.println((double) sum / count);
     }
 }
