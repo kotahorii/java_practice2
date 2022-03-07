bc9f406ab19a66d7598f072d28673371fa13e9c1
kotahorii <khorii.micron@gmail.com>
2022-03-07 14:59:05 +0900

    [Update] file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int a = 0;
-        int b = 1;
+        int size = scanner.nextInt();
 
-        for (int i = 1; i < n + 1; i++) {
-            if (i == 1) {
-                System.out.println(a);
-            } else if (i == 2) {
-                System.out.println(b);
-            } else {
-                int tmp = a;
-                a = b;
-                b += tmp;
-                System.out.println(b);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j || (size - i - 1) == j) {
+                    System.out.printf("X");
+                } else {
+                    System.out.printf(" ");
+                }
             }
+            System.out.println();
         }
     }
 }
