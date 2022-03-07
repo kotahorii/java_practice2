2156b490981f03ebdce83bf3c83cb26086b303b6
kotahorii <khorii.micron@gmail.com>
2022-03-07 13:33:20 +0900

    [Update] file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int print_count = scanner.nextInt();
+        String output_chars = "";
 
         for (int i = 0; i < print_count; i++) {
-            System.out.println("*");
+            output_chars += i % 10;
         }
+        System.out.println(output_chars);
     }
 }
