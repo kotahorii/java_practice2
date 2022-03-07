5f6060393d2b830a06d0a255edc131857e383609
kotahorii <khorii.micron@gmail.com>
2022-03-07 14:17:29 +0900

    [Update] file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         boolean is_stop = false;
         int sum = 0;
+        int count = 0;
         while (!is_stop) {
             int num = scanner.nextInt();
-            if (num == 0) {
+            if (num == 0 && count != 0) {
                 is_stop = true;
             } else {
                 sum += num;
+                count += 1;
             }
         }
-        System.out.println(sum);
+        System.out.println((double) sum / count);
     }
 }
