661a8a246744cac1facb2585634bf50618152668
kotahorii <khorii.micron@gmail.com>
2022-03-07 14:15:20 +0900

    [Update] file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
-                System.out.printf("%d * %d =%2d, ", j, i, i * j);
+        Scanner scanner = new Scanner(System.in);
+        boolean is_stop = false;
+        int sum = 0;
+        while (!is_stop) {
+            int num = scanner.nextInt();
+            if (num == 0) {
+                is_stop = true;
+            } else {
+                sum += num;
             }
-            System.out.println();
         }
+        System.out.println(sum);
     }
 }
