88a3b03d74f4eb265c22a428433529362a66ead8
kotahorii <khorii.micron@gmail.com>
2022-03-07 13:08:33 +0900

    [Update] file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int fact = 1;
-        for (int i = 7; i > 0; i--) {
-            fact *= i;
+        int count = 0;
+        Scanner scan = new Scanner(System.in);
+        int zero_count = 0;
+        int one_count = 0;
+        while (count < 10) {
+            int num = scan.nextInt();
+            if (num == 0) {
+                zero_count += 1;
+                count += 1;
+            } else if (num == 1) {
+                one_count += 1;
+                count += 1;
+            }
         }
-        System.out.println(fact);
+        System.out.println("勝ち" + one_count + "回 " + "負け" + zero_count + "回");
     }
 }
