a3d5dde5bf1a67dfe405f78a1ae00ae79921241b
kotahorii <khorii.micron@gmail.com>
2022-03-07 13:30:27 +0900

    [Update] file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,28 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int max_num = 0;
-        int min_num = 0;
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 10; i++) {
-            int input_num = scanner.nextInt();
-            if (max_num == 0) {
-                max_num = input_num;
-            } else {
-                if (input_num > max_num) {
-                    max_num = input_num;
-                }
-            }
+        int print_count = scanner.nextInt();
 
-            if (min_num == 0) {
-                min_num = input_num;
-            } else {
-                if (input_num < min_num) {
-                    min_num = input_num;
-                }
-            }
+        for (int i = 0; i < print_count; i++) {
+            System.out.println("*");
         }
-        System.out.println(max_num);
-        System.out.println(min_num);
     }
 }
